456595203c03ec2c0af70d8bff97f648c15175c1
MallikarjunKatkam <mallikarjuna.katkam@bhavnacorp.com>
2022-03-15 14:23:51 +0530

    EmployeeNaukriClientService

--- EmployeeNaukriCleintService/src/main/java/com/bhavna/model/EmployeeEntity.java
+++ EmployeeNaukriCleintService/src/main/java/com/bhavna/model/EmployeeEntity.java
@@ -1,15 +1,3 @@
-/**
- * "Create A Spring Boot Application, Use DB any of your choice
-DB will Have Employe Table with columns: id, Name, Age, Salary, Job Title, Gender
-
-Expose the below end points 
-
-* POST /api/Employee : To Save an Employee Details
-* GET /api/Employee/{id} : To retrie the Specific Employee Details with the id
-* PUT /api/Employee : To Update the Employee Details
-* DELETE /api/Employee/{id} : Will Delete the specific Employee with the id
-* GET /api/Employees?page=1&size=10 : Use Spring JPA Pagination concept here 
-The Specified URl will fetch First 10 records, and if page=2&size=10 it will fectch Records 11-20 in the DB"*/
 package com.bhavna.model;
 
 import javax.persistence.Column;
